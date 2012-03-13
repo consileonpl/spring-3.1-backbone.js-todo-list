1bec8018d8c25becfe9d1ab2e1ac16e72edf52d5
Michal Orman <michal.orman@gmail.com>
2012-03-13 17:10:25 +0100

    Updated web configuration.

--- src/main/java/pl/consileon/config/WebConfig.java
+++ src/main/java/pl/consileon/config/WebConfig.java
@@ -2,10 +2,16 @@ package pl.consileon.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "pl.consileon.web")
-public class WebConfig {
+@ComponentScan(basePackages = "pl.consileon")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
